eb7702d7f413ebf2b00ccd7e3901c0d8c3451e16
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-08-28 01:14:18 +0200

    ✨ creating navbar and footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import Footer from "./components/footer/Footer.tsx"
+import Navbar from "./components/nav/Navbar.tsx"
+
 function App() {
 
   return (
     <div className="h-lvh bg-slate-100">
-      <h1 className="text-3xl font-bold ">
-        Hello world!
-      </h1>
+      <Navbar/>
+      <h2 className="text-3xl font-bold ">
+        Welcome to our  website
+      </h2>
+      <Footer/>
     </div>
   )
 }
