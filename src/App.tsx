c31b282cb0115a2f9ac3e29cbf3730e5f91eb6b4
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-08-28 00:03:29 +0200

    🎉 Beginning project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="h-lvh bg-slate-100">
+      <h1 className="text-3xl font-bold ">
+      Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
