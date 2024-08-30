325640c1c6f9d980c0e241fe400a4d7056910ef7
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-08-30 17:05:40 +0200

    ✨ F08: Home / creating home page elements and creating ProductItem component

---
+++ src/components/product/ProductItem.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+
+const ProductItem = ({ product }) => {
+  return (
+    <div className="rounded border shadow-lg p-4 h-auto w-[220px] text-center ">
+      <img src={`http://localhost:4000/images/${product.imageFilename}`} alt="device"  className="object-contain h-48 w-96 my-1"/>
+      <hr />
+      <h4 className="my-3">{product.name}</h4>
+      <p>
+        Brand: {product.brand}, Category: {product.category} <br />
+        {product.description.substr(0, 50) + "..."}
+      </p>
+      <h4 className="my-3">{product.price}$</h4>
+      <Link role="button" className="btn btn-sm btn-primary" to={`/products/${product.id}`}>Details</Link>
+    </div>
+  )
+}
+
+export default ProductItem
