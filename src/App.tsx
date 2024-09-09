5ad9ffa622d2a60ffdc7bd987ac341959788c01c
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-09-09 18:59:36 +0200

    ✨ F15: progress in Login Page and Registration Page and creating AppContextProvider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,45 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Footer from "./components/footer/Footer.tsx"
-import Navbar from "./components/nav/Navbar.tsx"
-import Home from "./components/pages/home/Home.tsx"
-import Contact from "./components/pages/contact/Contact.tsx"
-import ErrorPage from "./components/pages/not-found/ErrorPage.tsx"
-import ProductList from "./components/pages/admin/products/ProductList.tsx"
-import CreateProduct from "./components/pages/admin/products/CreateProduct.tsx"
-import EditProduct from "./components/pages/admin/products/EditProduct.tsx"
-import ProductDetails from "./components/pages/admin/products/ProductDetails.tsx"
-import Register from "./components/pages/auth/Register.tsx"
-import Login from "./components/pages/auth/Login.tsx"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer.tsx";
+import Navbar from "./components/nav/Navbar.tsx";
+import Home from "./components/pages/home/Home.tsx";
+import Contact from "./components/pages/contact/Contact.tsx";
+import ErrorPage from "./components/pages/not-found/ErrorPage.tsx";
+import ProductList from "./components/pages/admin/products/ProductList.tsx";
+import CreateProduct from "./components/pages/admin/products/CreateProduct.tsx";
+import EditProduct from "./components/pages/admin/products/EditProduct.tsx";
+import ProductDetails from "./components/pages/admin/products/ProductDetails.tsx";
+import Register from "./components/pages/auth/Register.tsx";
+import Login from "./components/pages/auth/Login.tsx";
+import { AppContextProvider } from "./AppContext.tsx";
 
 function App() {
+  
 
   return (
     <div className="h-full bg-slate-100 container mx-auto">
-      <BrowserRouter>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="/products/:id" element={<ProductDetails/>}/>
+      <AppContextProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
 
-          <Route path="/auth/register" element={<Register/>}/>
-          <Route path="/auth/login" element={<Login/>}/>
-          
-          <Route path="/admin/products" element={<ProductList/>}/>
-          <Route path="/admin/products/create" element={<CreateProduct/>}/>
-          <Route path="/admin/products/edit/:id" element={<EditProduct/>}/>
-          <Route path="*" element={<ErrorPage/>}/>
-        </Routes>
-        <div className="static bottom-0 left-0 right-0">
-          <Footer/>
-        </div>
-      </BrowserRouter>
+            <Route path="/auth/register" element={<Register />} />
+            <Route path="/auth/login" element={<Login />} />
+
+            <Route path="/admin/products" element={<ProductList />} />
+            <Route path="/admin/products/create" element={<CreateProduct />} />
+            <Route path="/admin/products/edit/:id" element={<EditProduct />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+          <div className="static bottom-0 left-0 right-0">
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </AppContextProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
